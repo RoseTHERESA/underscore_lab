9e6fd4901d52d2936a621dcd748c23b35bb69c3e
Rose THERESA <rosetheresa@Roses-MacBook-Air.local>
2015-05-05 18:53:23 -0700

    fix typos

--- underscore.js
+++ underscore.js
@@ -2,82 +2,85 @@ var testArray = [45, 76, 22, 99];
 
 var _ = {
 
-        average: function(array) {
-            var sum = 0;
-            for (var i = 0; i < array.length; i++) {
-                sum += array[i];
-            }
+	average: function(array) {
+		var sum = 0;
+        for (var i = 0; i < array.length; i++) {
+        	sum += array[i];
+        }
             console.log(sum);
-            return (sum / array.length);
-        },
+        return (sum / array.length);
+    },
 
         console.log(_.average(testArray));
 
 
-        contains: function(array, number) {
-            for (var i = 0; i < array.length; i++) {
-                if (array[i] === number) {
-                    return true;
-                } else return false;
-            },
+    contains: function(array, number) {
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] === number) {
+                return true;
+            } 
+            else{
+            	return false;
+            }
+     },
 
-            console.log(_contains(testArray, 22));
-
-
-            first: function(array) {
-                return array[0];
-            },
-
-            console.log(_.first(testArray));
+        console.log(_contains(testArray, 22));
 
 
-            last: function(array) {
-                return array[array.length - 1];
-            },
+    first: function(array) {
+        return array[0];
+    },
 
-            console.log(_.last(testArray));
-
-
-            max: function(array) {
-                for (var i = 0; i < array.length; i++) {
-                    var max = 0;
-                    if (array[0] > array[1]) {
-                            max = array[0];
-                            return max
-                        }
-                    }
-                },
-
-                console.log(_.max(testArray));
+        console.log(_.first(testArray));
 
 
-                min: function(array) {
-                    for (var i = 0; i > array.length; i++) {
-                        var min = 0;
-                        if (array[0] < array[1]) {
-                                min = array[0];
-                                return min
-                            }
-                        }
-                    },
+    last: function(array) {
+         return array[array.length - 1];
+    },
 
-                    console.log
+        console.log(_.last(testArray));
 
 
-                 shuffle: function(array) {
-                 	for (var i = 0; i > array.length; i++)
-                 		var rand, temp;
-                 		rand = Math.floor((i + 1) * Math.random());
-                 		temp = array[rand];
-                 		array[rand] = 
+    max: function(array) {
+        for (var i = 0; i < array.length; i++) {
+            var max = 0;
+            if (array[0] > array[1]) {
+                max = array[0];               
+            }
+        }
+        return max
+    },
 
-                 	 {
-                 		Math.floor(Math.random)([0], [array.length - 1])
-                 		}
-                 		;
-                 		
-                 	}
-                 }
+        console.log(_.max(testArray));
+
+
+    min: function(array) {
+        for (var i = 0; i < array.length; i++) {
+            var min = 0;
+            if (array[0] < array[1]) {
+                min = array[0];               
+            }
+        }
+        return min
+    },
+
+        console.log(_min.(testArray));
+
+
+	shuffle: function(array) {
+        for (var i = 0; i < array.length; i++) {
+            var rand = Math.floor(Math.random() * (array.length - 1));
+            var temp = array[i];
+            array[i] = array[rand];
+            array[rand] = temp;
+        }
+      return testArray;
+    }
+
+};
+
+console.log(_.shuffle(testArray));
+          
 
 
 	/*sample:
